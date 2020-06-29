c0c06aded9182fe5a0aef1eda3790c7e02f567c5
Alex <Alex@DESKTOP-LTG9RQV>
2020-06-29 17:36:05 +0800

    有了Bean的Class类、修改了环境配置

---
+++ src/main/java/com/food/lehuo/bean/Order.java
@@ -0,0 +1,55 @@
+package com.food.lehuo.bean;
+
+import java.util.Date;
+
+public class Order {
+    private Integer oid;
+
+    private Integer uid;
+
+    private Date otime;
+
+    private Integer fid;
+
+    private Double totalprice;
+
+    public Integer getOid() {
+        return oid;
+    }
+
+    public void setOid(Integer oid) {
+        this.oid = oid;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Date getOtime() {
+        return otime;
+    }
+
+    public void setOtime(Date otime) {
+        this.otime = otime;
+    }
+
+    public Integer getFid() {
+        return fid;
+    }
+
+    public void setFid(Integer fid) {
+        this.fid = fid;
+    }
+
+    public Double getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(Double totalprice) {
+        this.totalprice = totalprice;
+    }
+}
---
+++ src/main/java/com/food/lehuo/dao/BlogMapper.java
@@ -0,0 +1,36 @@
+package com.food.lehuo.dao;
+
+import com.food.lehuo.bean.Blog;
+import com.food.lehuo.bean.BlogExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface BlogMapper {
+    long countByExample(BlogExample example);
+
+    int deleteByExample(BlogExample example);
+
+    int deleteByPrimaryKey(Integer bid);
+
+    int insert(Blog record);
+
+    int insertSelective(Blog record);
+
+    List<Blog> selectByExampleWithBLOBs(BlogExample example);
+
+    List<Blog> selectByExample(BlogExample example);
+
+    Blog selectByPrimaryKey(Integer bid);
+
+    int updateByExampleSelective(@Param("record") Blog record, @Param("example") BlogExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Blog record, @Param("example") BlogExample example);
+
+    int updateByExample(@Param("record") Blog record, @Param("example") BlogExample example);
+
+    int updateByPrimaryKeySelective(Blog record);
+
+    int updateByPrimaryKeyWithBLOBs(Blog record);
+
+    int updateByPrimaryKey(Blog record);
+}
